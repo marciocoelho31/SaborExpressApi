cbb74e1ea6e4d21524fd98465a891920300fcd48
Marcio Coelho Socorro <m.coelho.socorro@avanade.com>
2024-01-06 23:10:19 -0300

    Implementando herança e polimorfismo

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.sobremesa import Sobremesa
 
-restaurante_praca = Restaurante('praça', 'gourmet')
-restaurante_praca.receber_avaliacao('Marcio', 5)
-restaurante_praca.receber_avaliacao('Alex', 4)
-restaurante_praca.receber_avaliacao('Marcia', 2)
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+
+bebida_suco_caju = Bebida('Suco de Caju', 8.0, 'Grande')
+prato_sanduiche = Prato('Sanduiche', 11.0, 'Sanduiche Natural')
+sobremesa_sundae = Sobremesa('Sundae', 15.0, 'Sundae de Chocolate')
+
+bebida_suco_caju.aplicar_desconto()
+prato_sanduiche.aplicar_desconto()
+sobremesa_sundae.aplicar_desconto()
+
+restaurante_praca.adicionar_item_no_cardapio(bebida_suco_caju)
+restaurante_praca.adicionar_item_no_cardapio(prato_sanduiche)
+restaurante_praca.adicionar_item_no_cardapio(sobremesa_sundae)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
